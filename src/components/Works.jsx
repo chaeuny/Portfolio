b07855d12bd773fd992956c1abfafd21a0713e0a
Chaeun Yang <123149020+chaeuny@users.noreply.github.com>
2023-08-18 12:27:42 +0200

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,7 +1,7 @@
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
-import { styles } from "../styles";
+import { styles } from "../styles.js";
 import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
