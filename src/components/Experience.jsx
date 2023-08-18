0b518c062863af4c4b06f1b6299fdc6edc8330ae
Chaeun Yang <123149020+chaeuny@users.noreply.github.com>
2023-08-18 12:26:37 +0200

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 import 'react-vertical-timeline-component/style.min.css';
 
-import { styles } from '../styles';
+import { styles } from '../styles.js';
 import { experiences } from '../constants';
 import { SectionWrapper } from '../hoc';
 import { textVariant } from '../utils/motion';
